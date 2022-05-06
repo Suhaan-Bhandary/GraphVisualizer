1e196d8ac648a6f38a0bbd8bcacaecf360c1c9c6
Jayesh-Gavali <jngavali010120@kkwagh.edu.in>
2022-05-06 19:15:27 +0530

    Added Dijkstra's Animation

--- src/main/java/Main/Animation.java
+++ src/main/java/Main/Animation.java
@@ -130,7 +130,66 @@ public class Animation {
     }
 
     public static void animateDijkstra(int source, int destination, ArrayList<Integer> layer, ArrayList<Integer> reversePath, AnchorPane graphArea) {
-        // animation to be done
         System.out.println("animating dijkstra");
+        new Thread(() ->{
+            // Looping through each layer one by one
+            for (Integer node : layer) {
+                System.out.println(node);
+                // Selecting the circle and coloring it
+                Circle currNode = (Circle) graphArea.lookup("#circle__" + node);
+
+                // Color the nodes in DFS traversal
+                currNode.setFill(Color.CRIMSON);
+                currNode.setStroke(Color.BLACK);
+                currNode.setStrokeWidth(2);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                //making source and destination colors different for better visibility and resetting previous visited nodes
+                if (node == source) currNode.setFill(Color.YELLOW);
+                else if (node == destination) currNode.setFill(Color.LIGHTGREEN);
+                else {
+                    currNode.setFill(Color.LIGHTSKYBLUE);
+                    currNode.setStrokeWidth(0);
+                }
+
+            }
+
+            // After animating the nodes in layers, we will animate the path
+            for(int i = reversePath.size() - 2; i >= 0; i--)
+            {
+                // Getting the current and previous node in the path
+                int curr = reversePath.get(i), pre = reversePath.get(i + 1);
+
+                // Creating a store around the current node
+                Circle currNode = (Circle) graphArea.lookup("#circle__" + curr);
+                if(curr != source && curr != destination) currNode.setStroke(Color.ORANGE);
+                currNode.setStrokeWidth(2);
+
+                // Get the edge between the current and pre using the id, id can be in two forms pre-curr or curr-pre
+                Line currEdge = (Line) graphArea.lookup("#line__" + curr + "__" + pre);
+                if(currEdge == null)
+                {
+                    currEdge = (Line) graphArea.lookup("#line__" + pre + "__" + curr);
+                }
+
+                // Highlight the edge between the current and previous node in the path
+                if(currEdge != null) currEdge.setStroke(Color.ORANGE);
+
+                // Sleep for 500 millisecond after each path is highlighted
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
+
+
+
 }
